998a292dc9328877a1080b82dc34baa4ecbfe83e
ndr <andrekolmeijer@gmail.com>
2020-12-01 10:08:44 +0100

    Created home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+    <SelectMunicipality @select="select" />
+
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4 gap-4">
+        <DataCard :selectedMunicipality="state.selectedMunicipality"/>
+    </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { reactive } from 'vue';
+import DataCard from '@/components/DataCard.vue';
+import SelectMunicipality from '@/components/SelectMunicipality.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "Home",
+    components: { DataCard, SelectMunicipality },
+    setup() {
+        const state = reactive({
+            selectedMunicipality: 'GM0003'
+        })
+
+        function select(selected) {
+            state.selectedMunicipality = selected
+        }
+
+        return { state, select }
+    }
+};
 </script>
